22e0181c320c6cb538be654594d736bc4133a260
tarantino19 <tolentinored19@gmail.com>
2022-07-27 14:23:46 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+`use strict`;
+
+let homePoints = document.getElementById(`home-pts`);
+let homeCount = 0;
+let awayPoints = document.getElementById(`guest-pts`);
+let awayCount = 0;
+
+let resetScore = document.getElementById("home-pts "`guest-pts`);
+
+function homeAddPoint() {
+  homeCount += 1;
+  homePoints.innerText = homeCount;
+}
+
+function homeAddTwoPoints() {
+  homeCount += 2;
+  homePoints.innerText = homeCount;
+}
+
+function homeAddThreePoints() {
+  homeCount += 3;
+  homePoints.innerText = homeCount;
+}
+
+function guestAddPoint() {
+  awayCount += 1;
+  awayPoints.innerText = awayCount;
+}
+
+function guestAddTwoPoints() {
+  awayCount += 2;
+  awayPoints.innerText = awayCount;
+}
+
+function guestAddThreePoints() {
+  awayCount += 3;
+  awayPoints.innerText = awayCount;
+}
